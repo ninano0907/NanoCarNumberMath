5e06fe2e63cad471c565a8ee9e07ff99377c0ec2
KimJinWon <ninano0907@naver.com>
2020-05-08 18:02:16 +0900

    first commit

---
+++ CarNumberMath/MathUtil.swift
@@ -0,0 +1,13 @@
+//
+//  MathUtil.swift
+//  CarNumberMath
+//
+//  Created by Jin Won Kim on 2020/05/08.
+//  Copyright © 2020 Jin Won Kim. All rights reserved.
+//
+
+import UIKit
+
+class MathUtil: NSObject {
+
+}
