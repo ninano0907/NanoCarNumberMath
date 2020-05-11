76897ebb89be5cf8e765177cfa3f0fcafe65e0bc
KimJinWon <ninano0907@naver.com>
2020-05-11 09:19:36 +0900

    수정

--- CarNumberMath/MathUtil.swift
+++ CarNumberMath/MathUtil.swift
@@ -9,5 +9,124 @@
 import UIKit
 
 class MathUtil: NSObject {
-
+    
+    static func transformSign(i:Int) -> String {
+        switch i {
+        case 42:
+            return "*"
+        case 43:
+            return "+"
+        case 45:
+            return "-"
+        case 47:
+            return "/"
+        default:
+           return ""
+        }
+    }
+    static func makeSignArr() -> [Int] {
+        var calSignArr:[Int] = []
+        let calArr = [43, 45, 42, 47]
+        
+        for _ in 0...2 {
+            calSignArr.append(calArr[Int(arc4random_uniform(4))])
+        }
+        return calSignArr
+    }
+    
+    static func makeNumberArr(level:Int) -> [Int] {
+        var NumberArr:[Int] = []
+        let limit:UInt32 = UInt32(10 - level)
+        for _ in 0...3 {
+            NumberArr.append(Int(arc4random_uniform(limit)) + level)
+        }
+        
+        return NumberArr
+    }
+    
+    static func questionVerification(numberArr:[Int], signArr:[Int]) -> Bool {
+ 
+        var questionArr = [numberArr[0], signArr[0],
+                           numberArr[1], signArr[1],
+                           numberArr[2], signArr[2],
+                           numberArr[3]]
+        
+        var end = false
+        while !end {
+            var existSign = false
+            for i in 0...questionArr.count - 1 {
+                if i % 2 == 1 {
+                    print("* i :: \(i) = \(questionArr[i])")
+                    if questionArr[i] == 42 || questionArr[i] == 47 {
+                        
+                        let calNumber = cal(left: questionArr[i - 1], mid: questionArr[i], right: questionArr[i + 1])
+                        if questionArr[i] == 47 && calNumber == 0 {
+                            return false
+                        }
+                        questionArr.remove(at: i + 1)
+                        questionArr.remove(at: i)
+                        questionArr.remove(at: i - 1)
+                        
+                        questionArr.insert(calNumber, at: i - 1)
+                        
+                        existSign = true;
+                        break
+                    }
+                }
+            }
+            end = !existSign
+        }
+        return true
+    }
+    
+    
+    static func cal(left: Int, mid: Int, right: Int) -> Int {
+        switch mid{
+        case 43:
+            return left + right
+        case 45:
+            return left - right
+        case 42:
+            return left * right
+        case 47:
+            return left / right
+        default:
+            return 0
+        }
+    }
+    
+    
+    static func calcurate(numberArr:[Int], signArr:[Int], sign1:Int, sign2:Int) -> [Int]{
+        
+        var questionArr = [numberArr[0], signArr[0],
+                           numberArr[1], signArr[1],
+                           numberArr[2], signArr[2],
+                           numberArr[3]]
+        
+        var end = false
+        while !end {
+            var existSign = false
+            for i in 0...questionArr.count - 1 {
+                if i % 2 == 1 {
+                    print("* i :: \(i) = \(questionArr[i])")
+                    if questionArr[i] == sign1 || questionArr[i] == sign2 {
+                        
+                        let calNumber = MathUtil.cal(left: questionArr[i - 1], mid: questionArr[i], right: questionArr[i + 1])
+                        
+                        questionArr.remove(at: i + 1)
+                        questionArr.remove(at: i)
+                        questionArr.remove(at: i - 1)
+                        
+                        questionArr.insert(calNumber, at: i - 1)
+                        
+                        existSign = true;
+                        break
+                    }
+                }
+            }
+            end = !existSign
+        }
+        return questionArr
+    }
+    
 }
--- CarNumberMath/ViewController.swift
+++ CarNumberMath/ViewController.swift
@@ -9,12 +9,135 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myLbl: UILabel!
+    @IBOutlet weak var answerLbl: UILabel!
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBOutlet weak var num1Lbl: UILabel!
+    @IBOutlet weak var num2Lbl: UILabel!
+    @IBOutlet weak var num3Lbl: UILabel!
+    @IBOutlet weak var num4Lbl: UILabel!
+    
+    @IBOutlet weak var sign1Lbl: UILabel!
+    @IBOutlet weak var sign2Lbl: UILabel!
+    @IBOutlet weak var sign3Lbl: UILabel!
+    
+    
+//    var questionArr:[Int] = []
+    var numberArr:[Int] = []
+    var signArr:[Int] = []
+    
+    
+    var anwerSignArr:[Int] = []
+    
+    var answerNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makeQuiz()
     }
-
-
+    
+    func makeQuiz() {
+        
+        makeQuestion()
+        answerNumber = questionCal(numberArr: numberArr, signArr: signArr)
+        answerLbl.text = "\(answerNumber)"
+    }
+    
+    
+    
+    
+    
+    func makeQuestion() {
+         
+        numberArr = []
+        signArr = []
+        
+        numberArr = MathUtil.makeNumberArr(level: 1)
+        signArr = MathUtil.makeSignArr()
+        myLbl.text = "\(MathUtil.transformSign(i: signArr[0])), \(MathUtil.transformSign(i: signArr[1])), \(MathUtil.transformSign(i: signArr[2]))"
+        num1Lbl.text = "\(numberArr[0])"
+        num2Lbl.text = "\(numberArr[1])"
+        num3Lbl.text = "\(numberArr[2])"
+        num4Lbl.text = "\(numberArr[3])"
+         
+        
+//        questionArr = [numberArr[0], calSignArr[0],
+//                       numberArr[1], calSignArr[1],
+//                       numberArr[2], calSignArr[2],
+//                       numberArr[3]]
+        
+        if !MathUtil.questionVerification(numberArr: numberArr, signArr:signArr) {
+            makeQuestion()
+        }
+    }
+    
+    func questionCal(numberArr:[Int], signArr:[Int]) -> Int {
+        
+        var questionArr:[Int] = []
+        
+        questionArr = MathUtil.calcurate(numberArr: numberArr, signArr:signArr, sign1: 42, sign2: 47)
+        questionArr = MathUtil.calcurate(numberArr: numberArr, signArr:signArr, sign1: 43, sign2: 45)
+        
+        return questionArr[0]
+    }
+    
+    func insertAnser() {
+        if anwerSignArr.count == 0 {
+            sign1Lbl.text = ""
+            sign2Lbl.text = ""
+            sign3Lbl.text = ""
+        }
+        if anwerSignArr.count > 0 {
+            sign1Lbl.text = MathUtil.transformSign(i:anwerSignArr[0])
+        }
+        if anwerSignArr.count > 1 {
+            sign2Lbl.text = MathUtil.transformSign(i:anwerSignArr[1])
+        }
+        if anwerSignArr.count > 2 {
+            sign3Lbl.text = MathUtil.transformSign(i:anwerSignArr[2])
+        }
+    }
+    
+    
+    @IBAction func signAction(_ sender: UIButton) {
+        let tag = sender.tag
+        print("tag :: \(tag)")
+        if tag == 0 {
+            anwerSignArr.append(43)
+        } else if tag == 1 {
+            anwerSignArr.append(45)
+        } else if tag == 2 {
+            anwerSignArr.append(42)
+        } else if tag == 3 {
+            anwerSignArr.append(47)
+        }
+        insertAnser()
+    }
+    
+    @IBAction func initAction(_ sender: Any) {
+        anwerSignArr = []
+        insertAnser()
+        resultLbl.text  = "정답은?"
+    }
+    
+    @IBAction func newQuestionAction(_ sender: Any) {
+        makeQuiz()
+        resultLbl.text  = "정답은?"
+    }
+    @IBAction func sumitAction(_ sender: Any) {
+        
+        if answerNumber == questionCal(numberArr: numberArr, signArr: anwerSignArr) {
+            resultLbl.text = "정답!! \(answerNumber)"
+        } else {
+            resultLbl.text = "땡!!"
+        }
+        
+    }
+    
 }
 
